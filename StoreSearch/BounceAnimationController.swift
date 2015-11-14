9e73cd04e8c3d2b5aed9f0472f77ea99a8814f5e
RaghavMangrola <raghav.mangrola@gmail.com>
2015-11-14 12:38:12 -0800

    Playing around with rotation animations

--- StoreSearch/BounceAnimationController.swift
+++ StoreSearch/BounceAnimationController.swift
@@ -20,22 +20,27 @@ class BounceAnimationController: NSObject, UIViewControllerAnimatedTransitioning
       let toView = transitionContext.viewForKey(UITransitionContextToViewKey),
       let containerView = transitionContext.containerView() {
         
+        
         toView.frame = transitionContext.finalFrameForViewController(toViewController)
         containerView.addSubview(toView)
         toView.transform = CGAffineTransformMakeScale(0.7, 0.7)
         
         UIView.animateKeyframesWithDuration(transitionDuration(transitionContext), delay: 0, options: .CalculationModeCubic, animations: {
           UIView.addKeyframeWithRelativeStartTime(0.0, relativeDuration: 0.334, animations: {
-            toView.transform = CGAffineTransformMakeScale(1.2, 1.2)
+//            toView.transform = CGAffineTransformMakeScale(1.2, 1.2)
+            toView.transform = CGAffineTransformMakeRotation(CGFloat(0.7 * M_PI))
           })
           UIView.addKeyframeWithRelativeStartTime(0.334, relativeDuration: 0.333, animations: {
-            toView.transform = CGAffineTransformMakeScale(0.9, 0.9)
+//            toView.transform = CGAffineTransformMakeScale(0.9, 0.9)
+            toView.transform = CGAffineTransformMakeRotation(CGFloat(1.4 * M_PI))
           })
           UIView.addKeyframeWithRelativeStartTime(0.666, relativeDuration: 0.333, animations: {
-            toView.transform = CGAffineTransformMakeScale(1.0, 1.0)
+//            toView.transform = CGAffineTransformMakeScale(1.0, 1.0)
+            toView.transform = CGAffineTransformMakeRotation(CGFloat(2 * M_PI))
           })
           }, completion: { finished in
             transitionContext.completeTransition(finished)
+            
         })
     }
   }
